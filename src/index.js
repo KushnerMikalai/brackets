5488270d00dff1ef050d9cc1dfb1c42539a557ba
Nikolai_Kushner <n.kushner@imedia.by>
2019-03-05 14:13:41 +0300

    feat: tect complete!

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
+    let state = [];
 
-// function check(str, bracketsConfig) {
-
-
-    var state = [];
-
-    var brace_pairings = {},
-        brace_pairings_return = {};
-
-    bracketsConfig.map(item => {
-        brace_pairings[item[0]] = item[1];
-        brace_pairings_return[item[1]] = item[0];
-    });
-
-    // console.log(brace_pairings)
-
-    for(var i = 0; i < str.length; i++){
-
-        let x = brace_pairings[str[i]];
-
-        if (str[i] == x) {
-
-            // TODO
-            // return false;
-
-        } else {
-            if (str[i] == "(" || str[i] == "{" || str[i] == "[") {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] === bracketsConfig[j][1]){
+                if (state.length && state[state.length-1] === bracketsConfig[j][0]) {
+                    state.pop()
+                } else {
+                    state.push(str[i]);
+                }
+            } else if (str[i] === bracketsConfig[j][0]) {
                 state.push(str[i]);
-            } else if (str[i] in brace_pairings_return) {
-                if (state.pop() != brace_pairings_return[str[i]]) { return false; }
             }
         }
     }
+
     return !state.length;
 };
-
-
-// check('88888822225577877778775555666677777777776622222', [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']]);
